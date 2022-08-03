eb01f306c1a4bfa9df741678b7c0311fc35cd7c3
Dan Kiss <flowfast47@gmail.com>
2022-08-02 18:50:24 -0700

    refactor code

--- components/icons/anxietyIcon.tsx
+++ components/icons/anxietyIcon.tsx
@@ -1,11 +1,8 @@
 import Icon, { IconProps } from "./icon";
 import insomniaIcon from "../../public/mvp-icons/insomnia-icon.svg";
 
-const AnxietyIcon = ({
-  src = insomniaIcon,
-  alt = "icon",
-  height = "20px",
-  width = "20px",
-}: IconProps) => <Icon src={src} alt={alt} height={height} width={width} />;
+const AnxietyIcon = ({ src = insomniaIcon, alt, height, width }: IconProps) => (
+  <Icon src={src} alt={alt} height={height} width={width} />
+);
 
 export default AnxietyIcon;
--- components/icons/icon.tsx
+++ components/icons/icon.tsx
@@ -8,11 +8,18 @@ export type IconProps = {
   width?: string;
 };
 
+const defaultProps: IconProps = {
+  src: insomniaIcon,
+  alt: "icon",
+  height: "20px",
+  width: "20px",
+};
+
 const Icon = ({
-  src = insomniaIcon,
-  alt = "icon",
-  height = "20px",
-  width = "20px",
+  src = defaultProps.src,
+  alt = defaultProps.alt,
+  height = defaultProps.height,
+  width = defaultProps.width,
 }: IconProps) => <Image src={src} alt={alt} height={height} width={width} />;
 
 export default Icon;
